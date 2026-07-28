78701623c8378c6b1ecb4a4d73e3580e61b68182
Michał Krawczykowski <mkrawczykowski@gmail.com>
2026-07-28 11:16:32 +0200

    Initial project bootstrap

---
+++ main.py
@@ -0,0 +1,17 @@
+from oe import VERSION
+
+
+def main():
+    print()
+    print("===================================")
+    print("      Opierdalator Engine")
+    print("===================================")
+    print()
+    print(f"Version: {VERSION}")
+    print()
+    print("Project initialized.")
+    print()
+
+
+if __name__ == "__main__":
+    main()
